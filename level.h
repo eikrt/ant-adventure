72bff37d4c5262860ab50deb06e1d62742533140
Eino Korte <e.i.korte@gmail.com>
2022-01-29 16:16:56 +0200

    added some shader stuff

--- level.h
+++ level.h
@@ -50,7 +50,7 @@ class Level {
                     } 
 
                     if (bytes[k][i * levelSize + j] == '1'){
-                        this->objects.push_back(Object({(float) x ,(float) y,z},{1.0f,1.0f,1.0f}, models["cube"]));
+                        this->objects.push_back(Object({(float) x ,(float) y,z},{1.0f,1.0f,1.0f}, models["cube_0"]));
                         }
                         if (bytes[k][i * levelSize + j] == 'f') {
                             Entity entity = Entity(rand(), "roboant", "enemy", 0.03f, {(float)x,y+1.0f,z},{1.0f,0.5f,0.1f}, 1.0f, textures["roboant"]);
--- main.cpp
+++ main.cpp
@@ -37,10 +37,17 @@ int main(int argc, char* argv[])
             .up       = {0.0f, 1.0f, 0.0f},
             .fovy     = 50.0f,
     };
+
+    // fonts
+    Font fonts[1] = {0};
+    fonts[0] = LoadFont("fonts/romulus.png");
+    int levelAlpha = 255;
     int selectedButton = 0;
     map<string, Model> models;
     map<string, Texture2D> textures;
-    models["cube"] = LoadModel("models/cube.obj");
+    models["cube_0"] = LoadModel("models/cube.obj");
+    models["cube_1"] = LoadModel("models/cube.obj");
+    models["cube_2"] = LoadModel("models/cube.obj");
     models["ground"] = LoadModel("models/scenery.obj");
 
     textures["player"] = LoadTexture("res/ant_worker_side_1.png");
@@ -60,7 +67,9 @@ int main(int argc, char* argv[])
     mainMenuButtons.push_back(Button({32.0, 32.0}, buttonTextures));
     vector<Button> gameOverButtons;
     gameOverButtons.push_back(Button({32.0, 32.0}, buttonTextures));
-    models["cube"].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = textures["stoneBrick"];
+    models["cube_0"].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = textures["stoneBrick"];
+    models["cube_1"].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = textures["stoneBrick"];
+    models["cube_2"].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = textures["stoneBrick"];
     models["ground"].materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = textures["grass"];
     Entity player(0, "player", "player", 0.05f, {0.1f,8.0f,SCENEDIST},{0.4f,0.5f,0.1f}, 1.0f, textures["player"]);
     vector<Level> levels;
@@ -70,138 +79,144 @@ int main(int argc, char* argv[])
     scenes.push_back(Scenery({0.0f,-1.0f,0.0f},{200.0f,0.0f,200.0f}, models["ground"], textures));
     float gravity = 0.008f;
     RenderTexture2D target = LoadRenderTexture(WIDTH, HEIGHT);
-    while (!WindowShouldClose())
-    {
-        if (currentMode == mainMenu) {
-            if (IsKeyDown(KEY_DOWN)) {
-                if (selectedButton < mainMenuButtons.size() - 1) {
-                    selectedButton+=1;
-                    
-                }
-            }
-            if (IsKeyDown(KEY_UP)) {
-                if (selectedButton > 0) {
-                    selectedButton-=1;
-                }
-            }
-            if (IsKeyDown(KEY_SPACE)) {
-                if (selectedButton == 0) {
-                    currentMode = game;
-                }
-            }
-        BeginTextureMode(target);
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            mainMenuButtons[selectedButton].selected = true;
-            for (auto& b : mainMenuButtons) {
 
-                b.render(camera);
-                b.tick();
-            }
-            EndDrawing();
-            EndTextureMode();
-        }
-        else if (currentMode == gameOver) {
-            if (IsKeyDown(KEY_DOWN)) {
-                if (selectedButton < gameOverButtons.size() - 1) {
-                    selectedButton+=1;
-                    
+    // shaders
+        map<const char*, Shader> shaders;
+        shaders["default"] = LoadShader(TextFormat("shaders/base_lightning.vs"), TextFormat("shaders/lightning.fs"));
+        shaders["default"].locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shaders["default"], "viewPos");
+        models["cube_0"].materials[0].shader = shaders["default"];
+        while (!WindowShouldClose())
+        {
+            if (currentMode == mainMenu) {
+                if (IsKeyDown(KEY_DOWN)) {
+                    if (selectedButton < mainMenuButtons.size() - 1) {
+                        selectedButton+=1;
+                        
+                    }
                 }
-            }
-            if (IsKeyDown(KEY_UP)) {
-                if (selectedButton > 0) {
-                    selectedButton-=1;
+                if (IsKeyDown(KEY_UP)) {
+                    if (selectedButton > 0) {
+                        selectedButton-=1;
+                    }
                 }
-            }
-            if (IsKeyDown(KEY_SPACE)) {
-                if (selectedButton == 0) {
-                    exit(0);
+                if (IsKeyDown(KEY_SPACE)) {
+                    if (selectedButton == 0) {
+                        currentMode = game;
+                    }
                 }
+            BeginTextureMode(target);
+            BeginDrawing();
+                ClearBackground(RAYWHITE);
+                mainMenuButtons[selectedButton].selected = true;
+                for (auto& b : mainMenuButtons) {
+
+                    b.render(camera);
+                    b.tick();
+                }
+                EndDrawing();
+                EndTextureMode();
             }
-        BeginTextureMode(target);
-        BeginDrawing();
-            ClearBackground(RAYWHITE);
-            gameOverButtons[selectedButton].selected = true;
-            for (auto& b : gameOverButtons) {
+            else if (currentMode == gameOver) {
+                if (IsKeyDown(KEY_DOWN)) {
+                    if (selectedButton < gameOverButtons.size() - 1) {
+                        selectedButton+=1;
+                        
+                    }
+                }
+                if (IsKeyDown(KEY_UP)) {
+                    if (selectedButton > 0) {
+                        selectedButton-=1;
+                    }
+                }
+                if (IsKeyDown(KEY_SPACE)) {
+                    if (selectedButton == 0) {
+                        exit(0);
+                    }
+                }
+            BeginTextureMode(target);
+            BeginDrawing();
+                ClearBackground(RAYWHITE);
+                gameOverButtons[selectedButton].selected = true;
+                for (auto& b : gameOverButtons) {
 
-                b.render(camera);
-                b.tick();
+                    b.render(camera);
+                    b.tick();
+                }
+                EndDrawing();
+                EndTextureMode();
+
             }
-            EndDrawing();
-            EndTextureMode();
-
-        }
-        else if (currentMode == game) {
-        if (player.getPos().x - camera.position.x > 10.0) {
-        
-            camera.position.x = player.getPos().x + 10;
-            camera.target.x = player.getPos().x + 10;
-        }
-        else if (player.getPos().x - camera.position.x < -10.0) {
-            camera.position.x = player.getPos().x - 10;
-            camera.target.x = player.getPos().x - 10;
-        }
-
-        player.tick(gravity);
-        if (player.getHp() <= 0) {
-            currentMode = gameOver;
-        }
-
-        for (auto &e : levels[currentLevel].entities) {
-            if (e.getHp() <= 0) {
-                continue;
+            else if (currentMode == game) {
+            if (player.getPos().x - camera.position.x > 10.0) {
+            
+                camera.position.x = player.getPos().x + 10;
+                camera.target.x = player.getPos().x + 10;
+            }
+            else if (player.getPos().x - camera.position.x < -10.0) {
+                camera.position.x = player.getPos().x - 10;
+                camera.target.x = player.getPos().x - 10;
             }
 
+            player.tick(gravity);
+            if (player.getHp() <= 0) {
+                currentMode = gameOver;
+            }
+
+            for (auto &e : levels[currentLevel].entities) {
+                if (e.getHp() <= 0) {
+                    continue;
+                }
+
+                for (auto &obj : levels[currentLevel].getObjects()) {
+                    if (obj.getHp() <= 0) {
+                        continue;
+                    }
+                    e.collision_object(obj);
+                }
+                e.tick(gravity);
+            }
             for (auto &obj : levels[currentLevel].getObjects()) {
                 if (obj.getHp() <= 0) {
                     continue;
                 }
-                e.collision_object(obj);
+                player.collision_object(obj);
             }
-            e.tick(gravity);
-        }
-        for (auto &obj : levels[currentLevel].getObjects()) {
-            if (obj.getHp() <= 0) {
-                continue;
-            }
-            player.collision_object(obj);
-        }
-        for (auto &e : levels[currentLevel].entities) {
-            if (e.getHp() <= 0) {
-                continue;
-            }
-            player.collision_entity(e);
-        }
-       // player.collision_object(ground);
-        //player.collision_object(cube);
-        BeginTextureMode(target);
-        {
-            ClearBackground(SKYBLUE);
-
-            if (IsKeyDown(KEY_SPACE))
-                player.jump();
-
-            if (IsKeyDown(KEY_D))
-                player.right();
-            if (IsKeyDown(KEY_A))
-                player.left();
-            if (!IsKeyDown(KEY_A) && !IsKeyDown(KEY_D))
-                player.slowX();
-            if (IsKeyPressed(KEY_W)) {
-                player.back();
-            }
-            if (IsKeyPressed(KEY_S)) {
-                player.forward();
-            }
-            BeginMode3D(camera);
-                for (auto &obj : levels[currentLevel].getObjects()) {
-
-                    if (obj.getHp() <= 0) {
-                        continue;
-                    }
-                    obj.render(camera);
+            for (auto &e : levels[currentLevel].entities) {
+                if (e.getHp() <= 0) {
+                    continue;
                 }
-                for (auto &e : levels[currentLevel].getEntities()) {
+                player.collision_entity(e);
+            }
+           // player.collision_object(ground);
+            //player.collision_object(cube);
+            BeginTextureMode(target);
+            {
+                ClearBackground(SKYBLUE);
+
+                if (IsKeyDown(KEY_SPACE))
+                    player.jump();
+
+                if (IsKeyDown(KEY_D))
+                    player.right();
+                if (IsKeyDown(KEY_A))
+                    player.left();
+                if (!IsKeyDown(KEY_A) && !IsKeyDown(KEY_D))
+                    player.slowX();
+                if (IsKeyPressed(KEY_W)) {
+                    player.back();
+                }
+                if (IsKeyPressed(KEY_S)) {
+                    player.forward();
+                }
+                BeginMode3D(camera);
+                    for (auto &obj : levels[currentLevel].getObjects()) {
+
+                        if (obj.getHp() <= 0) {
+                            continue;
+                        }
+                        obj.render(camera);
+                    }
+                    for (auto &e : levels[currentLevel].getEntities()) {
 
                     if (e.getHp() <= 0) {
                         continue;
@@ -220,6 +235,15 @@ int main(int argc, char* argv[])
         BeginDrawing();
             //DrawTextureRec(target.texture, (Rectangle){0, 0, (float)WIDTH, (float)-HEIGHT}, Vector2{0, 0}, WHITE);
             DrawTexturePro(target.texture, (Rectangle){0, 0, (float)WIDTH, (float)-HEIGHT},{0,0,(float)screenWidth, (float)screenHeight} ,Vector2{0, 0}, 0, WHITE);
+            if (levelAlpha > 0) {
+                levelAlpha -= 2;
+            }
+            else {
+                levelAlpha = 0;
+            }
+            if (levelAlpha > 0) {
+            DrawTextEx(fonts[0], "Ruins", {screenWidth / 2 - 16,screenHeight / 2},18, 2, {255,255,255, levelAlpha});
+            }
         EndDrawing();
     }
     
